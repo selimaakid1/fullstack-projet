0d5e7f99a228cefbedab6509cfa7fc2689f9b69f
Ben Ali Slim <slouma97@hotmail.fr>
2020-05-15 20:47:17 +0100

    Update OurFeatures.js

--- client/src/components/OurFeatures.js
+++ client/src/components/OurFeatures.js
@@ -14,7 +14,7 @@ const OurFeatures = () => {
                             <img class="card-img-top" src="https://images.unsplash.com/photo-1581546104493-f7e013a136ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Card image cap" />
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <p class="card-text">Somes quick example text to build on the card title and make up the bulk of the card's content.</p>
 
                             </div>
                         </div>
