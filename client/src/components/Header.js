25bb6be43550fe91336ab49509bb2d80c607706d
selimaakid1 <selima.akid@yahoo.com>
2020-05-15 22:08:31 +0200

    Menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
     return (
         <div className='header-image'>
-           
+           <h1>L'art de vous acceuillir</h1>
         </div>
     )
 }
--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -9,6 +9,80 @@ function Menu() {
             <h1 className='menu-title'>Menu à la carte</h1>
             <p className='menu-text'>
                 Explorez la texture, la couleur et bien sûr les goûts ultimes avec notre menu de saison. Tous les ingrédients sont frais et soigneusement sélectionnés par nos chefs. Profitez d'une expérience culinaire extraordinaire.</p>
+                <div>
+                    <h1>Hors d'oeuvre</h1>
+            <div>
+                <h5>Assiette de Fruits et Fondue de Brie</h5>
+                <p>Wedge of baked Brie with ripe melon and berries</p>
+            </div>
+            <hr></hr>
+            <div>
+                <h5>Chausson de Fromage de Chèvre</h5>
+                <p>Goat cheese tarts</p>
+            </div>
+            <hr></hr>
+            <div>
+                <h5>Palourdes au Gratin</h5>
+                <p>Baked clams with garlic butter and bread crumbs</p>
+            </div>
+            <hr></hr>
+        </div>
+        <div>
+            <h1>Soupe et Salades</h1>
+            <div>
+                <h5>Soupe du Jour</h5>
+                <p>Ask your server for our chef's daily soup selection</p>
+            </div>
+            <hr></hr>
+            <div>
+                <h5>Soupe à l'Oignon</h5>
+                <p>Classic onion soup, made with three types of onions and topped with melted gruyère</p>
+            </div>
+            <hr></hr>
+            <div>
+                <h5>Epinards et Concombres à la Grecque</h5>
+                <p>Spinach salad with feta and yogurt garlic dressing</p>
+            </div>
+            <hr></hr>
+            <div>
+                <h5>Salade Niçoise</h5>
+                <p>Organic greens, flaked tuna, niçoise olives, anchovies, egg, tomatoes and bell peppers</p>
+            </div>
+            <hr></hr>
+            <div>
+                <h5>Salade de Canard</h5>
+                <p>Roasted duck with orange sherry vinaigrette, pecans, bacon and red onions on seasonal greens</p>
+            </div>
+            <hr></hr>
+        </div>
+        <div>
+            <h1>Sandwiches</h1>
+            <div>
+                <h5>Fromage au Jambon</h5>
+                <p>Roasted ham and gruyere with aioli on a baguette</p>
+            </div>
+            <hr></hr>
+            <div>
+                <h5>Poulet</h5>
+                <p>Grilled chicken breast with lemon caper sauce, tomato, lettuce, and gruyere on focaccia</p>
+            </div>
+            <hr></hr>
+            <div>
+                <h5>Turkey Pesto</h5>
+                <p>Thin sliced baked turkey with pickled red onions, lettuce, tomato, and fresh basil pesto on a baguette</p>
+            </div>
+            <hr></hr>
+            <div>
+                <h5>Crêpe Saumon Fumé</h5>
+                <p>Smoked salmon, pickled red onions, and gruyere melted in a housemade buckwheat crepe</p>
+            </div>
+            <hr></hr>
+            <div>
+                <h5>Crêpe Parisian</h5>
+                <p>Thin sliced roasted ham and gruyere in a housemade buckwheat crepe</p>
+            </div>
+            <hr></hr>
+        </div>
         </div>
     )
 }
