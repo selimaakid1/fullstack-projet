4654857e0c6ce7d02e47dc386ee2186b418361ef
selimaakid1 <selima.akid@yahoo.com>
2020-05-16 17:28:25 +0200

    Fix menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div>
             <div className='header-image'>
-                <h1>L'art de vous acceuillir</h1>
+               
             </div>
             <div className='frontImg'>
                 <button type="button" class="btn btn-secondary reservNow">Make Your Reservation Now</button>
--- client/src/components/RestaurantInfo.js
+++ client/src/components/RestaurantInfo.js
@@ -4,28 +4,31 @@ const RestoInfo = () => {
     return (
 
         <div className='container ' >
+            <h2 className='small-title'>-Welcome-</h2>
             <div className='infoTitle'>
-                <h1>The restaurant </h1>
-                <h4>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labo</h4>
+                <h1 className='resto-title'>Le restaurant </h1>
+                <h4 className='resto-text'>L'art de vous acceuillir</h4>
             </div>
             <div className='inline'>
                 <div className='Info'>
 
-                    <h4>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labo</h4>
+                    <h4 className='resto-text-1'>Chez ..., il n'y a d'autres ambition que celle de proposer une cuisine sincère, ancrée dans les saisons, authentique mais moderne joyeux et genereuse.
+                        Arrosée de bons vins nous la voulons au choix degustée, avalée, dévorée, engloutie dans une ambiance decontractée, simple et bon enfant.
+                    </h4>
                 </div>
                 <div className='col-md-4'>
                     <div className='column'>
                         <div className='row'>
                             <div className='col-xs-12'>
-                                <img className='imgMod1' src='https://demo.web3canvas.com/themeforest/tomato/img/thumb1.png' />
+                                <img className='imgMod1' src='https://i.pinimg.com/564x/b3/3e/63/b33e63e229b518dd5473774b8ae7387d.jpg' />
                             </div>
                         </div>
                         <div className='row columntwo '>
                             <div className='col-sm-6'>
-                                <img className='imgcoltwo' src='https://demo.web3canvas.com/themeforest/tomato/img/thumb2.png' />
+                                <img className='imgcoltwo' src='https://i.pinimg.com/564x/f9/5e/96/f95e968d0c0a22cb78678c97b345a7fd.jpg' />
                             </div>
                             <div className='col-sm-6 '>
-                                <img className='imgcoltwo' src='https://demo.web3canvas.com/themeforest/tomato/img/thumb2.png' />
+                                <img className='imgcoltwo' src='https://i.pinimg.com/564x/2e/bf/2d/2ebf2dc8b9f91c3ebb34c1e638671b85.jpg' />
                             </div>
                         </div>
                     </div>
