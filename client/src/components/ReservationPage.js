ae720b0975ffee91f43a101aaa21c719df80a8c0
selimaakid1 <selima.akid@yahoo.com>
2020-05-19 21:59:57 +0200

    Update

--- client/src/components/ReservationPage.js
+++ client/src/components/ReservationPage.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Reservation from './Reservation'
 import ReservationList from './ReservationList'
 
-function ReservationPage() {
-    return (
+class ReservationPage extends Component {
+    componentWillMount() {
+        { this.props.loadUser() }
+     }
+     render(){
+
+     return (
         <div>
             <div className='reservation-header'>
                 <h1>Reservation</h1>
@@ -13,5 +18,5 @@ function ReservationPage() {
         </div>
     )
 }
-
+}
 export default ReservationPage
