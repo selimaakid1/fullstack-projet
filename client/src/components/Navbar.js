32a9d9860a87eb8ef579d068c089aaa12899d974
Ben Ali Slim <slouma97@hotmail.fr>
2020-06-03 10:34:42 +0100

    Chart / responsive Home

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,55 +5,55 @@ import { logout } from '../actions/AuthActions'
 const Navbar = (props) => {
 
     const userConnected = () => (
+            <div className='row d-flex '>
+                <div className='col' id='links'>
+                    <Link to='/'>Acceuil</Link>
+                </div>
+                <div className='col' id='links'>
+                    <Link to='/menu'>Menu</Link>
+                </div>
+                <div i className='col' id='links' >
+                    <Link to='/reservation'>Reservation</Link>
+                </div>
+                <div className='col' id='links'>
+                    <Link to='/contact'>Contact</Link>
+                </div>
 
-        <div className='row d-flex'>
-            <div className='col' id='links'>
-                <Link to='/'>Acceuil</Link>
-            </div>
-            <div className='col' id='links'>
-                <Link to='/menu'>Menu</Link>
-            </div>
-            <div i className='col' id='links' >
-                <Link to='/reservation'>Reservation</Link>
-            </div>
-            <div className='col' id='links'>
-                <Link to='/contact'>Contact</Link>
-            </div>
-
-            <div className='col' className='dropdown' id='links'>
-                <i class="fas fa-caret-down"></i>
-                <div class="dropdown-content">
-                    <div className='col' className='username' id='links'>
-                        {props.auth.user ? props.auth.user.FirstName + ' ' + props.auth.user.LastName : ''}
-                    </div>
-                    <div className='col'>
-                        <a href="#!" onClick={props.logout}>
-                            <i className="fas fa-sign-out-alt"></i>
+                <div className='col' className='dropdown' id='links'>
+                    <i class="fas fa-caret-down"></i>
+                    <div class="dropdown-content">
+                        <div className='col' className='username' id='links'>
+                            {props.auth.user ? props.auth.user.FirstName + ' ' + props.auth.user.LastName : ''}
+                        </div>
+                        <div className='col'>
+                            <a href="#!" onClick={props.logout}>
+                                <i className="fas fa-sign-out-alt"></i>
                             Logout
                     </a>
-                        
+
+                        </div>
+
                     </div>
-                    
                 </div>
             </div>
-        </div>
-
+       
 
     )
 
     const guest = () => (
-        <div className='row d-flex'>
-            <div className='col' id='links'>
-                <Link to='/'>Acceuil</Link>
+        
+            <div className='row d-flex'>
+                <div className='col' id='links'>
+                    <Link to='/'>Acceuil</Link>
+                </div>
+                <div className='col' id='links'>
+                    <Link to='/register'>Inscription</Link>
+                </div>
+                <div i className='col' id='links' >
+                    <Link to='/login'>Login</Link>
+                </div>
             </div>
-            <div className='col' id='links'>
-                <Link to='/register'>Inscription</Link>
-            </div>
-            <div i className='col' id='links' >
-                <Link to='/login'>Login</Link>
-            </div>
-
-        </div>
+        
     )
 
     return (
